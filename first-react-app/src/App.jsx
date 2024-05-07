f4ac62804549cf9423afe2afd84048813add6d0e
Rudresh Patre <rudreshpatre@gmail.com>
2024-05-07 10:56:00 +0530

    Basic Jsx syntax variations

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+
+
 const App = () => {
+  const name = "Rudresh";
+  var x = 10;
+  var y = 20;
+  var names = ['Brad','Chad','Nate','Kate'];
+  var loggedIn = false;
   return (
     <div>
         <div className="text-5xl"> App</div>
-        <p>Hello</p>
+        <p style={{color:'red', fontSize:'24px'}}>Hello {name}</p>
+        <p>Sum of {x} and {y} is {x+y}</p>
+        <ul>
+          {names.map((name)=>(
+            <li>{name}</li>
+          ))}
+        </ul>
+        {loggedIn && <h1>Hello Member!</h1>}
     </div>
   );   
 };
