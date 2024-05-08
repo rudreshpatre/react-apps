8092c3fc8565a2f880178927dc5efe72154c0411
Rudresh Patre <rudreshpatre@gmail.com>
2024-05-08 11:20:29 +0530

    Add Hero component

--- first-react-app/src/components/Navbar.jsx
+++ first-react-app/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/images/logo.png'
 
-const Navbar = () => {
-    return (
+const Navbar = () =>{
+    return(
         <nav className="bg-indigo-700 border-b border-indigo-500">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex h-20 items-center justify-between">
