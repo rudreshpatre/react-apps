68dfcf2f7bad55474665231e7abbac491c900d12
Rudresh Patre <rudreshpatre@gmail.com>
2024-05-12 10:16:59 +0530

    Another way to use props within component

--- first-react-app/src/components/Hero.jsx
+++ first-react-app/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-const Hero = (props) => {
+const Hero = ({title,subtitle}) => {
  return(
     //   {/* <!-- Hero --> */}
      <section className="bg-indigo-700 py-20 mb-4">
@@ -9,10 +9,10 @@ const Hero = (props) => {
          <h1
            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
          >
-           {props.title}
+           {title}
          </h1>
          <p className="my-4 text-xl text-white">
-           {props.subtitle}
+           {subtitle}
          </p>
        </div>
      </div>
