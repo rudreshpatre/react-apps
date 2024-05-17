f835f41afcfd5ab00b4320580adf1fc78649d7ce
Rudresh Patre <rudreshpatre@gmail.com>
2024-05-17 12:23:27 +0530

    Use hook useState to set state for toggling between less or more description

--- first-react-app/src/components/JobListing.jsx
+++ first-react-app/src/components/JobListing.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
 const JobListing = ({job}) => {
+
+  const[showFullDescription, setShowFullDescription] = useState(false);
+  let description = job.description;
+  if(!showFullDescription){
+    description = description.substring(0,90) + '...';
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
     <div className="p-4">
@@ -10,8 +18,10 @@ const JobListing = ({job}) => {
         </div>
 
         <div className="mb-5">
-            {job.description}    
+            {description}    
         </div>
+
+        <button onClick={() => setShowFullDescription((prevState) => !prevState)} className='text-indigo-500 mb-5 hover:text-indigo-60'>{showFullDescription ? 'Less' : 'More'}</button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
