9f9520e6518a4a19a454a4d834a7387c341a2863
Rudresh Patre <rudreshpatre@gmail.com>
2024-05-21 18:36:02 +0530

    Setup router
    add boiler plate code for router, add layout and pages using outlet with layout

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import {
+Route,
+createBrowserRouter,
+createRoutesFromElements,
+RouterProvider
+} from 'react-router-dom';
+import React from 'react';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />}/>
+  </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-    <Navbar />
-        <Hero />
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
-    </>
-    );   
+  return <RouterProvider router={router} />; 
 };
 
 export default App;
